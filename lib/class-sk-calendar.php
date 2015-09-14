dc0688c62816609cc3d3f77c44fe5518eafe5b88
dansod <info@dcweb.nu>
2015-09-14 16:18:04 +0200

    fixed link to date events from calendar puff

--- lib/class-sk-calendar.php
+++ lib/class-sk-calendar.php
@@ -86,10 +86,8 @@
             $cell_content = $this->current_day;
 
             if( in_array( $this->current_day, $this->occupied_days ) ) {
-            	
-            	$cell_content = '<a href="/events/list/?tribe-bar-date=' . $this->current_year . '-' . $this->current_month . '-' . $this->current_day . '">' . $cell_content . '</a>';
-            	$lastest_day = $this->current_day;
-
+            	$cell_content = '<a href="/events/' . $this->current_year . '-' . $this->current_month . '-' . $this->current_day . '/">' . $cell_content . '</a>';
+              $lastest_day = $this->current_day;
             } 
             $this->current_day++;   
             
