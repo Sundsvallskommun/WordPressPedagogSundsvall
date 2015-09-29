11b63e5e3bc98e576fa587974f95a87d0abd6986
mariantonsson <mari.antonsson@cybercom.com>
2015-09-29 14:33:00 +0200

    Added a h5, div-class sk-sidebar-panel-wrapper and sk-sidebar-panel-list

--- sidebar-reportage.php
+++ sidebar-reportage.php
@@ -1,13 +1,17 @@
+<div class="sk-sidebar-panel-wrapper">
 
   <nav class="of-sidebar-menu">
     <?php $panels = get_field('sk_panels', $post->ID); ?>
     <?php if(! empty( $panels ) ) : ?>
-    	
+    	<h5><?php _e('Fördjupning', 'sk'); ?></h5>
     <?php foreach( $panels as $panel ) : ?>
     	<div class="sk-sidebar-panel">
-    	<div class="sidebar-panel-title"><?php echo $panel['sk_panel_header']; ?></div>
+    	<div class="sk-sidebar-panel-title"><?php echo $panel['sk_panel_header']; ?></div>
+      <div class="sk-sidebar-panel-list">
     	<?php echo $panel['sk_panel_content']; ?>
+      </div>
     	</div>
     <?php endforeach; ?>
   <?php endif; ?>
   </nav>
+</div>
