f487e789d723e8195726266b473a2080a966d68b
dansod <info@dcweb.nu>
2015-10-01 23:18:17 +0200

    added css overrides to use base theme css for calendar event

--- tribe-events/default-template.php
+++ tribe-events/default-template.php
@@ -20,7 +20,9 @@ get_header(); ?>
   	<div class="of-c-sm-4 of-c-md-3">
 			<div id="tribe-events-pg-template">
 				<?php tribe_events_before_html(); ?>
+				<div class="sk-entry-content">
 				<?php tribe_get_view(); ?>
+				</div>
 				<?php tribe_events_after_html(); ?>
 			</div> <!-- #tribe-events-pg-template -->
 			<?php comments_template( '', true );  ?>
