468079d310fcc74d3a433e17d770978d543ecb80
Daniel Söderström <daniel.soderstrom@cybercom.com>
2015-11-20 08:11:59 +0100

    changed slug name for organisationsniva to verksamhet

--- lib/helpers/single-reportage.php
+++ lib/helpers/single-reportage.php
@@ -23,7 +23,7 @@ function get_article_panels(){
 	<h5><?php _e('Relaterade inlägg', 'sk'); ?></h5>
 	<?php
 
-	get_article_panel_by_global_category( $parent_category = array( 'organisationsniva' ), $panel_title = __( 'Verksamhet', 'sk' ), $offset = '-1' );
+	get_article_panel_by_global_category( $parent_category = array( 'verksamhet' ), $panel_title = __( 'Verksamhet', 'sk' ), $offset = '-1' );
 	get_article_panel_by_global_category( $parent_category = array( 'pedagogik', 'amne' ), $panel_title = __( 'Ämne & pedagogik', 'sk' ), $offset = '-1' );
 	get_article_panel_by_global_category( $parent_category = array(), $panel_title = __( 'Bloggar', 'sk' ), $offset = 1, $only_subsites = '1' );
 	get_calendar_panel_by_global_category( $parent_category = array(), $panel_title = __( 'Kalender', 'sk' ), $offset = 1, $only_subsites = '1' );
