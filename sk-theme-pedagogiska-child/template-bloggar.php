c7b922fa60d84f1bbd9e39c0e88fd9e16a86b151
Daniel Söderström <daniel.soderstrom@cybercom.com>
2016-08-31 12:03:02 +0200

    Fix for WP_Site now returning object instead of an array.

--- sk-theme-pedagogiska-child/template-bloggar.php
+++ sk-theme-pedagogiska-child/template-bloggar.php
@@ -11,11 +11,12 @@ $args  = array(
 $sites = get_sites( $args );
 
 foreach ( $sites as $key => $s ) {
-	switch_to_blog( $s['blog_id'] );
-	$sites[ $key ]['name']        = get_bloginfo( 'name' );
-	$sites[ $key ]['description'] = get_bloginfo( 'description' );
 
-	$sites[ $key ]['image'] = wp_get_attachment_image_src( get_field( 'sk-blog-image', 'options' ), 'thumbnail' );
+	switch_to_blog( $s->blog_id );
+	$sites[ $key ]->name        = get_bloginfo( 'name' );
+	$sites[ $key ]->description = get_bloginfo( 'description' );
+
+	$sites[ $key ]->image = wp_get_attachment_image_src( get_field( 'sk-blog-image', 'options' ), 'thumbnail' );
 	restore_current_blog();
 }
 ?>
@@ -37,24 +38,24 @@ foreach ( $sites as $key => $s ) {
 					<ul class="sk-grid-list">
 						<?php foreach ( $sites as $site ) : ?>
 							<li>
-								<a href="<?php switch_to_blog( $site['blog_id'] ); ?><?php echo get_bloginfo( 'url' ); ?><?php restore_current_blog(); ?>"
+								<a href="<?php switch_to_blog( $site->blog_id ); ?><?php echo get_bloginfo( 'url' ); ?><?php restore_current_blog(); ?>"
 								   class="of-dark-link">
 
-									<?php if ( ! empty( $site['image'] ) ) : ?>
+									<?php if ( ! empty( $site->image ) ) : ?>
 										<figure>
-											<img width="<?php echo $site['image'][1]; ?>"
-											     height="<?php echo $site['image'][2]; ?>"
-											     alt="<?php echo $site['description']; ?>"
+											<img width="<?php echo $site->image[1]; ?>"
+											     height="<?php echo $site->image[2]; ?>"
+											     alt="<?php echo $site->description; ?>"
 											     class="attachment-thumbnail wp-post-image"
-											     src="<?php echo $site['image'][0]; ?>">
+											     src="<?php echo $site->image[0]; ?>">
 										</figure>
 									<?php endif; ?>
 
 									<article class="sk-narrow">
 										<header>
-											<h5><?php echo $site['name']; ?></h5>
+											<h5><?php echo $site->name; ?></h5>
 										</header>
-										<p><?php echo $site['description']; ?></p>
+										<p><?php echo $site->description; ?></p>
 									</article>
 								</a>
 							</li>
